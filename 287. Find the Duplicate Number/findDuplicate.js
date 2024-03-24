4953e3a206919962e40c79dd7c9f038de04ab652
avanesyans <sergey.avanesyan@dowjones.com>
2024-03-24 11:04:23 -0400

    feat: find the duplicate number

---
+++ 287. Find the Duplicate Number/findDuplicate.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findDuplicate = (nums) => {
+  let slow = 0;
+  let fast = 0;
+
+  while (true) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+    if (slow === fast) {
+      break;
+    }
+  }
+
+  let slow2 = 0;
+  while (true) {
+    slow = nums[slow];
+    slow2 = nums[slow2];
+    if (slow === slow2) {
+      break;
+    }
+  }
+
+  return slow;
+};
+
+const nums = [1, 3, 4, 2, 2];
+console.log('findDuplicate: ', findDuplicate(nums))
