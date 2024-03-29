5d7cbe40cd2681617f6f249662f497c40fbc5565
avanesyans <sergey.avanesyan@dowjones.com>
2024-03-29 13:05:33 -0400

    feat: longest substring with at least k repeating characters

---
+++ 395. Longest Substring with At Least K Repeating Characters/longestSubstring.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+const longestSubstring = (s, k) => {
+
+};
+
+const s = "aaabb"; 
+const k = 3;
+console.log('longestSubstring: ', longestSubstring(s, k));
