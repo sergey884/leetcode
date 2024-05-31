cabbfc009b25d84f605bf3b7c8b905f6c1386633
avanesyans <sergey.avanesyan@dowjones.com>
2024-05-30 22:57:04 -0400

    feat: implement a couple of leetcode tasks

---
+++ 1137. N-th Tribonacci Number/README.md
@@ -0,0 +1,76 @@
+# [1137. N-th Tribonacci Number](https://leetcode.com/problems/n-th-tribonacci-number/description/?)
+
+The Tribonacci sequence $T_n$ is defined as follows: 
+
+$T_0$ = 0, $T_1$ = 1, $T_2$ = 1, and $T_{n+3}$ = $T_n$ + $T_{n+1}$ + $T_{n+2}$ for n >= 0.
+
+Given `n`, return the value of $T_n$.
+
+
+### Example 1:
+> Input: n = 4
+>
+> Output: 4
+>
+> Explanation:
+>
+> $T_3$ = 0 + 1 + 1 = 2
+>
+> $T_4$ = 1 + 1 + 2 = 4
+
+
+### Example 2:
+> Input: n = 25
+>
+> Output: 1389537
+ 
+
+### Constraints:
+- `0 <= n <= 37`
+- The answer is guaranteed to fit within a 32-bit integer, ie. answer <= $2^{31}$ - 1.
+
+
+## Solutions
+
+### Javascript
+```javascript
+const tribonacci = (n) => {
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+
+  const res = Array(n + 4).fill(0);
+  res[1] = 1;
+  res[2] = 1;
+
+  for (let i = 0; i < n - 2; i++) {
+    res[i + 3] = res[i] + res[i + 1] + res[i + 2];
+  }
+
+  return res[n];
+};
+```
+
+### Python
+```python
+class Solution:
+  def tribonacci(self, n: int) -> int:
+    if n == 0:
+      return 0
+    
+    if n == 1 or  n == 2:
+      return 1
+  
+    res = [0] * (n + 4)
+    res[1] = 1
+    res[2] = 1
+  
+    for i in range(0, n - 2):
+      res[i + 3] = res[i] + res[i + 1] + res[i + 2]
+
+    return res[n]
+```
---
+++ 1971. Find if Path Exists in Graph/validPath.js
@@ -0,0 +1,67 @@
+class UnionFind {
+  constructor(n) {
+    this.id = [];
+    for (let i = 0; i < n; i++) {
+      this.id[i]= i;
+    }
+    this.rank = Array(n).fill(1);
+  }
+
+  union(u, v) {
+    const rootU = this.find(u);
+    const rootV = this.find(v);
+
+    if (rootU === rootV) {
+      return;
+    }
+
+    if (this.rank[rootU] < this.rank[rootV]) {
+      this.id[rootU] = rootV;
+    } else if (this.rank[rootU] > this.rank[rootV]) {
+      this.id[rootV] = rootU;
+    } else {
+      this.id[rootU] = rootV;
+      this.rank[rootV]++;
+    }
+  }
+
+  find(u) {
+    if (this.id[u] !== u) {
+      this.id[u] = this.find(this.id[u])
+    }
+
+    return this.id[u];
+  }
+}
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+const validPath = (n, edges, source, destination) => {
+    const uf = new UnionFind(n);
+
+    for (let i = 0; i < edges.length; i++) {
+      const [u, v] = edges[i];
+      uf.union(u, v);
+    }
+    console.log('uf.find(source): ', uf.find(source));
+    console.log('uf.find(destination): ', uf.find(destination));
+    return uf.find(source) === uf.find(destination);
+};
+
+// const n = 3;
+// const edges = [[0, 1], [1, 2], [2, 0]];
+// const source = 0;
+// const destination = 2;
+
+const n = 6;
+const edges = [[0,1],[0,2],[3,5],[5,4],[4,3]];
+const source = 0;
+const destination = 5;
+
+
+console.log('validPath: ', validPath(n, edges, source, destination));
