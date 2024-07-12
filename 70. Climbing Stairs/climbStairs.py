82456025193bb31ccf3a23d59c06772cd4728553
avanesyans <sergey.avanesyan@dowjones.com>
2024-07-11 23:15:40 -0400

    feat: Climbing Stairs

---
+++ 70. Climbing Stairs/climbStairs.py
@@ -0,0 +1,13 @@
+class Solution:
+  def climbStairs(self, n: int) -> int:
+    dp = [1, 1] + [0] * (n - 1)
+
+    for i in range(2, n + 1):
+      dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+n = 3
+solution = Solution()
+print("climbStairs: ", solution.climbStairs(n))
