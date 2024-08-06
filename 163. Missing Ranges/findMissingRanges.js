abfa2b4a4a05312cfe09fdff81d200445fd90e73
Sergey Avanesyan <ser.avanesyan@gmail.com>
2024-08-06 17:43:19 -0400

    [163] feat: Missing Ranges

---
+++ 163. Missing Ranges/findMissingRanges.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {number[][]}
+ */
+const findMissingRanges = (nums, lower, upper) => {
+  if (!nums || !nums.length) {
+    return [[lower, upper]];
+  }
+
+  const missingRanges = [];
+  if (lower < nums[0]) {
+    missingRanges.push([lower, nums[0] - 1]);
+  }
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] + 1 !== nums[i + 1]) {
+      missingRanges.push([nums[i] + 1, nums[i + 1] - 1]);
+    }
+  }
+
+  if (upper > nums[nums.length - 1]) {
+    missingRanges.push([nums[nums.length - 1] + 1, upper]);
+  }
+
+  return missingRanges;
+};
+
+
+const nums = [0, 1, 3, 50, 75];
+const lower = 0;
+const upper = 99;
+// Output: [[2,2], [4,49], [51,74], [76,99]]
+console.log('findMissingRanges: ', findMissingRanges(nums, lower, upper));
+     
