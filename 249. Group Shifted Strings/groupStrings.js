1c638d33c9f0ca8636e5ef8c8d89b2d80b10e729
Sergey Avanesyan <ser.avanesyan@gmail.com>
2024-08-06 09:24:45 -0400

    [249] feat: Group Shifted Strings

---
+++ 249. Group Shifted Strings/groupStrings.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} strings
+ * @return {string[][]}
+ */
+const groupStrings = (strings) => {
+  const group = {};
+  const shiftLetter = (letter, shift) => {
+    const chCode = (letter.charCodeAt(0) - shift) % 26 + 'a'.charCodeAt(0);
+    return chCode;
+  }
+
+  const getHash = (str) => {
+    const shift = str.charCodeAt(0);
+    const strCodes = [...str].map(it => shiftLetter(it, shift));
+    return String.fromCharCode(...strCodes);
+  }
+
+  for (let i = 0; i < strings.length; i++) {
+    const key = getHash(strings[i]);
+    if (!group[key]) {
+      group[key] = [];
+    }
+    group[key].push(strings[i])
+  }
+
+  return Object.values(group)
+};
+
+
+const strings = ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"];
+console.log('groupStrings: ', groupStrings(strings));
